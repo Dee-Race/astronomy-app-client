9692c1306e5cf20c76bf12014a36b94994a1b7b0
Dee Race <deerace12@gmail.com>
2018-10-19 13:36:37 -0400

    add fetchAstronomy function to fetch api data, add navbar and astronomy desc components

---
+++ src/components/AstronomyDesc.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AstronomyDescription = props => {
+  return <div>{props.data.explanation}</div>;
+};
+
+export default AstronomyDescription;
--- src/containers/AstronomyContainer.js
+++ src/containers/AstronomyContainer.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import AstronomyDescription from "../components/AstronomyDesc";
 
 class AstronomyContainer extends Component {
+  state = {
+    astronomy: []
+  };
+
+  fetchAstronomy = () => {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`;
+    fetch(`${url}`)
+      .then(resp => resp.json())
+      .then(data => this.setState({ astronomy: data.results }));
+    console.log(this.state.astronomy);
+  };
+
+  componentDidMount() {
+    this.fetchAstronomy();
+  }
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <AstronomyDescription />
+      </div>
+    );
   }
 }
+
+export default AstronomyContainer;
