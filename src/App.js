ea662f4d87e7b150d29ef360e4334dcab919c917
Dee Race <deerace12@gmail.com>
2018-10-23 17:31:38 -0400

    add mdbreact navbar, edit navbar text and color

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import AstronomyContainer from "./containers/AstronomyContainer";
-import Header from "./components/Navbar";
+import NavbarFeatures from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Astronomy App</h1>
-        <Header className="Header" />
+        <NavbarFeatures className="Navbar" />
         <div className="main-content">
           <AstronomyContainer />
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,85 @@
 import React, { Component } from "react";
-import { Navbar, Nav, NavItem } from "react-bootstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarNav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "mdbreact";
+import { BrowserRouter as Router } from "react-router-dom";
 
-class Header extends Component {
+class NavbarFeatures extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
+
   render() {
     return (
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">ASTRONOMY APP</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Home
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            About
-          </NavItem>
-        </Nav>
-      </Navbar>
+      <Router>
+        <Navbar color="indigo" dark expand="md" scrolling>
+          <NavbarBrand href="/">
+            <strong>Astronomy App</strong>
+          </NavbarBrand>
+          {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+          <Collapse isOpen={this.state.collapse} navbar>
+            <NavbarNav left>
+              <NavItem active>
+                <NavLink to="#">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#">Create a Galaxy</NavLink>
+              </NavItem>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle nav caret>
+                    Dropdown
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem href="#">Action</DropdownItem>
+                    <DropdownItem href="#">Another Action</DropdownItem>
+                    <DropdownItem href="#">Something else here</DropdownItem>
+                    <DropdownItem href="#">Something else here</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+            <NavbarNav right>
+              <NavItem>
+                <form className="form-inline md-form mt-0">
+                  <input
+                    className="form-control mr-sm-2 mb-0 text-white"
+                    type="text"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </form>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+        </Navbar>
+      </Router>
     );
   }
 }
 
-export default Header;
+export default NavbarFeatures;
