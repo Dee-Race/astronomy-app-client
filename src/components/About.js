19eada82747d9e48ff5ea3300989d75065a2fa4e
Dee Race <deerace12@gmail.com>
2018-11-07 14:52:45 -0500

    add lengthier about page description

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,13 @@ import React from "react";
 const AboutPage = () => {
   return (
     <div>
-      <p>About Astronomy App!</p>
+      <p>
+        Get inspired by NASA's Astronomy Picture of the Day to save and record
+        information on locations, dates, and content. Are you inspired by an
+        image and it's location to plan a trip? Are you fascinated by the new
+        astronomy information and want to further research a concept? Visit the
+        AstroApp daily to view outstanding images and create a new AstroNote!
+      </p>
     </div>
   );
 };
