51f15a70c1f4a8e21949372aa357b84a270dfe2f
Dee Race <deerace12@gmail.com>
2018-12-06 17:42:35 -0500

    create notes reducer

--- src/store.js
+++ src/store.js
@@ -1,3 +1,13 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 
 import thunk from "redux-thunk";
+
+const notesReducer = (state = [], action) => {
+  switch (action.type) {
+    case "GET_NOTE":
+      return action.notes;
+
+    default:
+      return state;
+  }
+};
