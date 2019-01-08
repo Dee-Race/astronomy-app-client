4ab045e2bbde66ba49dd516c784c1eec596217d8
Dee Race <deerace12@gmail.com>
2019-01-08 17:08:42 -0500

    add my reducer notes to the noteLikes.js reducer file

--- src/reducers/noteLikes.js
+++ src/reducers/noteLikes.js
@@ -13,3 +13,7 @@ export default function noteLikesReducer(state = initialState, action) {
       return state;
   }
 }
+
+// Reducer function accepts two arugments, state and action - and returns the
+// next version of the state. When the reducer recieves the INCREMENT_LIKES action, it
+// should return state + 1
